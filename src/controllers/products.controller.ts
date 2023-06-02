1acc0308f77359d9d55e70e05d9ffaf274552e46
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-06-02 10:43:41 -0500

    Metodos http get post put delete

--- src/controllers/products.controller.ts
+++ src/controllers/products.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Query,
+  Post,
+  Body,
+  Put,
+  Delete,
+} from '@nestjs/common';
 
 @Controller('products')
 export class ProductsController {
@@ -8,16 +17,47 @@ export class ProductsController {
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return `limit ${limit}, offset ${offset}, brand ${brand}`;
+    return {
+      message: `limit ${limit}, offset ${offset}, brand ${brand}`,
+    };
   }
 
   @Get('filter')
   getProductFilter() {
-    return `yo soy filter`;
+    return {
+      message: `yo soy filter`,
+    };
   }
 
   @Get(':productId')
   getProduct(@Param('productId') productId: any) {
-    return `product ${productId}`;
+    return {
+      message: `product ${productId}`,
+    };
+  }
+
+  @Post()
+  create(@Body() payload: any) {
+    return {
+      message: 'Accion de crear',
+      payload,
+    };
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() payload: any) {
+    return {
+      id,
+      payload,
+    };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return {
+      id,
+      delete: true,
+      count: 1,
+    };
   }
 }
