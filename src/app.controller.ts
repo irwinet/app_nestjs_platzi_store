44a5409f967164e1ab582665a664c1367c820dd9
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-06-02 09:57:27 -0500

    Http get - routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -18,5 +18,29 @@ export class AppController {
   @Get('/ruta/')
   hello() {
     return 'con /sas/';
+  }
+
+  @Get('products')
+  getProducts(
+    @Query('limit') limit = 100,
+    @Query('offset') offset = 0,
+    @Query('brand') brand: string,
+  ) {
+    return `limit ${limit}, offset ${offset}, brand ${brand}`;
+  }
+
+  @Get('products/filter')
+  getProductFilter() {
+    return `yo soy filter`;
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: any) {
+    return `product ${productId}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('productId') productId: any, @Param('id') id: any) {
+    return `product ${productId} and category ${id}`;
   }
 }
