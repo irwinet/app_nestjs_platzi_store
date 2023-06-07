c4e7d9102a0f0231ee3aa828a40c011348aa0ac4
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-06-07 17:49:13 -0500

    Uso de useValue y useClass

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { UsersModule } from './users/users.module';
 
+const API_KEY = '12345';
+const API_KEY_POD = '54321';
+
 @Module({
   imports: [ProductsModule, UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue: process.env.NODE_ENV === 'prod' ? API_KEY_POD : API_KEY,
+    },
+  ],
 })
 export class AppModule {}
