fc3d1693978e4146c4ecf74f3f9aac9be55fe676
ext_iestrada <ext_iestrada@tottus.com.pe>
2023-06-08 12:57:11 -0500

    Uso de Tipado en config

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,18 +1,22 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
+import { ConfigService, ConfigType } from '@nestjs/config';
+import config from './config';
 
 @Injectable()
 export class AppService {
   constructor(
     // @Inject('API_KEY') private apiKey: string,
     @Inject('TASKS') private tasks: any[],
+    @Inject(config.KEY) private configService2: ConfigType<typeof config>,
     private configService: ConfigService,
   ) {}
 
   getHello(): string {
     console.log(this.tasks);
-    const apiKey = this.configService.get<string>('API_KEY');
-    const dbName = this.configService.get('DATABASE_NAME');
+    // const apiKey = this.configService.get<string>('API_KEY');
+    // const dbName = this.configService.get('DATABASE_NAME');
+    const apiKey = this.configService2.apiKey;
+    const dbName = this.configService2.database.name;
     return `Hello World! ${apiKey} - ${dbName}`;
   }
 }
