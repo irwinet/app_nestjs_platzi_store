1f029741ed0af23c961c93aa3149e284e4f3c6f1
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-05-23 20:25:07 -0500

    Add entity OrderItem - Order

--- src/users/entities/order-item.entity.ts
+++ src/users/entities/order-item.entity.ts
@@ -1,19 +1,16 @@
+import { Product } from 'src/products/entities/product.entity';
 import {
   PrimaryGeneratedColumn,
   Entity,
   CreateDateColumn,
   UpdateDateColumn,
+  Column,
   ManyToOne,
-  OneToMany,
 } from 'typeorm';
-
-// import { Product } from 'src/products/entities/product.entity';
-// import { User } from './user.entity';
-import { Customer } from './customer.entity';
-import { OrderItem } from './order-item.entity';
+import { Order } from './order.entity';
 
 @Entity()
-export class Order {
+export class OrderItem {
   @PrimaryGeneratedColumn()
   id: number;
 
@@ -29,9 +26,12 @@ export class Order {
   })
   updateAt: Date;
 
-  @ManyToOne(() => Customer, (customer) => customer.orders)
-  customer: Customer;
+  @Column({ type: 'int' })
+  quantity: number;
 
-  @OneToMany(() => OrderItem, (item) => item.order)
-  items: OrderItem[];
+  @ManyToOne(() => Product)
+  product: Product;
+
+  @ManyToOne(() => Order, (order) => order.items)
+  order: Order;
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -7,9 +7,14 @@ import { ProductsModule } from 'src/products/products.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Customer } from './entities/customer.entity';
+import { Order } from './entities/order.entity';
+import { OrderItem } from './entities/order-item.entity';
 
 @Module({
-  imports: [ProductsModule, TypeOrmModule.forFeature([User, Customer])],
+  imports: [
+    ProductsModule,
+    TypeOrmModule.forFeature([User, Customer, Order, OrderItem]),
+  ],
   providers: [UsersService, CustomersService],
   controllers: [UsersController, CustomersController],
 })
